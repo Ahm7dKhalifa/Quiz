56405ee514ffa9fdf4b61b70a2275d4346130eff
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-04-16 20:51:27 +0200

    add ouizpdf database and put the three image target in the _main scene

---
+++ QuizUnity/Assets/_APP/Scripts/AnimationByVB.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class AnimationByVB : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+    public string aniName;
+    // Use this for initialization
+    void Start()
+    {
+        //vbBtnObj = GameObject.Find("VirtualButtonPlay");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        cubeAni.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play(aniName);
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("Button released");
+    }
+}
---
+++ QuizUnity/Assets/_APP/Scripts/Prefabs/VirtualButtonEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualButtonEvents : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+
+    // Use this for initialization
+    void Start()
+    {
+        //vbBtnObj = GameObject.Find("VirtualButtonPlay");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        cubeAni.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("RotateCube");
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("Button released");
+    }
+}
