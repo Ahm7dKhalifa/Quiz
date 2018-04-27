26ab440e29bb62b9d16d74731b69c066bff5c301
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-04-27 19:01:08 +0200

    complete first prototype as you well see in video

---
+++ QuizUnity/Assets/_APP/Scripts/DisplayVideo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class DisplayVideo : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject parent;
+    public GameObject vbBtnObj;
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+   
+    //EnableAllChild
+    public void EnableAllChild()
+    {
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            var child = parent.transform.GetChild(i).gameObject;
+            if (child != null)
+                child.SetActive(true);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        EnableAllChild();
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+       
+        Debug.Log("Button released");
+    }
+}
---
+++ QuizUnity/Assets/_APP/Scripts/StopVideo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class StopVideo : MonoBehaviour, IVirtualButtonEventHandler
+{ 
+
+    public GameObject parent;
+    public GameObject vbBtnObj;
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    //DisableAllChild
+    public void DisableAllChild()
+    {
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            var child = parent.transform.GetChild(i).gameObject;
+            if (child != null)
+                child.SetActive(false);
+        }
+    }
+   
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        DisableAllChild();
+        Debug.Log("Button released");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+        Debug.Log("Button released");
+    }
+}
